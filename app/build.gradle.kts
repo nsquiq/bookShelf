fe1c8254d92e8feb43a89fb6ce29f3801312fc0b
Nuray <210103331@stu.sdu.edu.kz>
2023-11-29 20:21:58 +0600

    data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")  version "1.8.10"
 }
 
 android {
@@ -60,6 +61,29 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+
+
+    // retrofit
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // json file
+    //implementation "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1"
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // images in json
+    implementation("io.coil-kt:coil-compose:2.2.2")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+
+    implementation("androidx.compose.material:material-icons-extended:1.3.1")
+
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
---
+++ app/src/main/java/com/example/bookshelf/model/Book.kt
@@ -0,0 +1,50 @@
+package com.example.bookshelf.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Book(
+    val id: String,
+    val description: String,
+    val volumeInfo: VolumeInfo,
+
+) {
+
+
+}
+
+@Serializable
+data class VolumeInfo(
+    val title: String,
+    val subtitle: String,
+    val description: String,
+    val imageLinks: ImageLinks? = null,
+    val authors: List<String>,
+    val publisher: String,
+    val publishedDate: String,
+) {
+    val allAuthorsx: String
+        get() = allAuthors()
+
+    fun allAuthors() : String {
+        var x= ""
+        for (author in authors) {
+            x += "$author, "
+        }
+        return x.trimEnd(',', ' ')
+    }
+}
+
+@Serializable
+data class ImageLinks(
+    val smallThumbnail: String,
+    val thumbnail: String,
+) {
+    val httpsThumbnail : String
+        get() = thumbnail.replace("http", "https")
+}
+
+
+
+
+
